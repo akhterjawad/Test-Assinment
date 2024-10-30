e0e49d8fa27862487905c919db74ca1f65d6b6f3
Muhammad Jawad <akhterjawad566@gmail.com>
2024-10-30 20:06:47 +0500

    working on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,23 +2,29 @@ import React from 'react'
 import { HiOutlineMail } from "react-icons/hi";
 import { MdOutlinePhone } from "react-icons/md";
 
-
-
 const Navbar = () => {
   return (
-    <section className='bg-[#388FCB] w-[100%]'>
-      <div className='container mx-auto p-4 text-white'>
-            <div className='flex justify-between items-center'>
-                <div className='flex gap-10'>
-                    <h1 className='flex items-center gap-2'><HiOutlineMail  className='text-[22px]'/> info@abc.com</h1>
-                    <h1 className='flex items-center gap-2'><MdOutlinePhone  className='text-[22px]'/> +123 456 789</h1>
-                </div>
-                <div>
-                    <h1>Privacy Policy | Terms and Condition</h1>
-                </div>
+    <React.Fragment>
+      <section className='w-[100%] bg-[#388FCB]'>
+        <div className='text-white container mx-auto p-4'>
+          <div className='items-center flex justify-between'>
+            <div className='flex gap-10'>
+              <h1 className='flex items-center gap-2'>
+                <HiOutlineMail className='text-[22px]' />
+                info@abc.com
+              </h1>
+              <h1 className='flex items-center gap-2'>
+                <MdOutlinePhone className='text-[22px]' />
+                +123 456 789
+              </h1>
             </div>
-      </div>
-    </section>
+            <div>
+              <h1>Privacy Policy | Terms and Condition</h1>
+            </div>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
   )
 }
 
